4a752e13b43c3d02eb328b27ba8ce13dba1f4223
se1yu <jeslynchang80@gmail.com>
2026-06-25 23:32:22 -0500

    Add GitHub Pages deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/William-Zuo-Personal-Website',
+  assetPrefix: '/William-Zuo-Personal-Website/',
 }
 
 module.exports = nextConfig
